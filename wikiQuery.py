f11d4261daa6c7169ef527eb82671a4e931a37a2
Zakariaa Hamid <35637740+deadrix00@users.noreply.github.com>
2018-04-13 11:08:22 -0400

    Add files via upload

---
+++ wikiQuery.py
@@ -0,0 +1,44 @@
+import os,requests,json
+from StringIO import StringIO
+
+if os.name == "nt": os.system("cls")
+else:	os.system("clear")
+
+print "\t\t\tWikipediaSearchQuery by Zakariaa Hamid :v"
+print "\nfile path : "+os.getcwd()+"/"+__file__
+print "UID : "+str(os.getuid())
+print "GID : "+str(os.getgid())
+print "PID : "+str(os.getpid())
+print "\n"
+
+
+usage = "USAGE:\npython "+__file__+" [SEARCH QUERY]"
+try:
+	while 1:
+		searchQuery = str(raw_input("enter a search query : "))
+		print "\n"
+		wikiLink = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+searchQuery
+		wikiId = "https://en.wikipedia.org/?curid="
+		jsonReq = requests.get(wikiLink).content
+		io = StringIO(jsonReq)
+		jsonLoad = json.load(io)
+		for a in jsonLoad["query"]["pages"]:
+
+			urlId = jsonLoad["query"]["pages"][a]["pageid"]
+			urlIdOpen = requests.get(wikiId+str(urlId))
+			urlIdOpen = urlIdOpen.url
+			title = jsonLoad["query"]["pages"][a]["title"]
+			print title
+
+			urltitle = "_".join(title.split(" "))
+			urlIdOpen = "https://en.wikipedia.org/wiki/"+urltitle
+			print urlIdOpen
+
+			print jsonLoad["query"]["pages"][a]["extract"]
+			print "\n"
+
+except KeyboardInterrupt, e:
+	print e
+	print "GOOD BYE DUDE :)"
+except:
+	print usage
